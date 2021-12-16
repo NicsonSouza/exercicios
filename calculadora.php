a2cc27073097f88e76c11a78bf298f2cc4171ecb
NicsonSouza <nick.nicson@gmail.com>
2021-12-16 05:47:39 -0300

    05.47

--- calculadora.php
+++ calculadora.php
@@ -48,7 +48,6 @@
     7* Resolva um Exercício em PHP que receba os valores A, B, C e D. Efetue a soma entre A e C, a multiplicação entre B e D e verifique se o resultado da soma é maior, menor ou igual ao da multiplicação. Imprima: “A+C é maior que B+D”, “A+C é menor que B+D”, “A+C é igual a B+D”.
     8* Crie um formulário em HTML que receba Nome, Ano de Nascimento e Sexo, e faça com que o PHP imprima o Nome, sexo e a idade;
 
-
     OBS.: Forma de enviar, crie uma pasta atividade02, e uma pasta para cada questão como uma index.php, depois transforme em rar. e anexe aqui na atividade. -->
 </body>
 
